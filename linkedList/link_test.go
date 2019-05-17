b854f50278614222f9b2830d56e800cae592a2b4
妙计 <zhangjian@mioji.com>
2019-05-17 14:54:52 +0800

    fix

--- linkedList/link_test.go
+++ linkedList/link_test.go
@@ -3,3 +3,20 @@
 * @Date: 2019/5/17 下午2:44
 */
 package linkedList
+
+import (
+	"testing"
+)
+
+func TestLink(t *testing.T) {
+	List := NewLinkedList()
+	List.InsertToTail(1)
+	List.InsertToTail(2)
+	List.InsertToTail(3)
+	List.InsertToTail(4)
+	List.InsertToTail(5)
+	List.Print()
+	List.InsertToHead(111)
+	List.InsertToHead(222)
+	List.Print()
+}
