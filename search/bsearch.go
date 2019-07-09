384d3dc7253b21e5bf6ade1e63c833743e0bc4ef
妙计 <zhangjian@mioji.com>
2019-07-09 14:42:41 +0800

    fix

--- search/bsearch.go
+++ search/bsearch.go
@@ -40,3 +40,96 @@ func search(a []int, searchdate, low, high int) int {
 		return search(a, searchdate, mid + 1, high)
 	}
 }
+
+//二分变形 查找第一个符合的值的元素
+func BsearchFirst(a []int, searchNum int) int {
+	low := 0
+	high := len(a) - 1
+	//防止提前跳出循环
+	for low <= high {
+		//用+法可能会导致数据溢出 位运算会比
+		mid := low + ((high - low) >> 1)
+
+		if searchNum == a[mid] {
+			if (mid == 0) || (a[mid-1] != searchNum) {
+				return mid
+			} else {
+				high = mid - 1
+			}
+		} else if searchNum < a[mid] {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
+
+//查找最后一个符合要求的元素
+func BsearchEnd(a []int, searchNum int) int {
+	low := 0
+	high := len(a) - 1
+	//防止提前跳出循环
+	for low <= high {
+		//用+法可能会导致数据溢出 位运算会比
+		mid := low + ((high - low) >> 1)
+
+		if searchNum == a[mid] {
+			if (mid == 0) || (a[mid+1] != searchNum) {
+				return mid
+			} else {
+				low = mid + 1
+			}
+		} else if searchNum < a[mid] {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
+
+//查找大于给定值的元素
+func BsearchEqFirst(a []int, searchNum int) int {
+	low := 0
+	high := len(a) - 1
+	//防止提前跳出循环
+	for low <= high {
+		//用+法可能会导致数据溢出 位运算会比
+		mid := low + ((high - low) >> 1)
+		if searchNum > a[mid] {
+			if (mid == 0) || (a[mid-1] < searchNum) {
+				return mid
+			} else {
+				low = mid - 1
+			}
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
+
+//搜索旋转排序数组
+//思路：先找到旋转点(即最小值),再利用二分查找
+func SearchAb(a []int) int {
+	//先找到旋转点 复杂度logn
+	lens := len(a)
+	low := 0
+	high := lens - 1
+
+	for low <= high {
+		pivot := low + (high - low) / 2
+
+		if a[pivot] <= a[0] {
+			if  pivot == 0 || a[pivot - 1] > a[0] {
+				return pivot
+			} else {
+				high = pivot - 1
+			}
+		} else {
+			low = pivot + 1
+		}
+	}
+	return -1
+}
--- search/bsearch_test.go
+++ search/bsearch_test.go
@@ -18,3 +18,35 @@ func TestBsearch2(t *testing.T) {
 	a := []int{1,2,3,4,5,6,7,8,9}
 	fmt.Println(Bsearch2(a, 3))
 }
+
+func TestBsearchFirst(t *testing.T) {
+	a := []int{1,2,3,4,4,4,4,6,7,7,7,5,6,7,8,9}
+	fmt.Println(BsearchFirst(a, 4))
+}
+
+func TestBsearchEnd(t *testing.T) {
+	a := []int{1,2,3,4,4,4,4,6,7,7,5,6,7,8,9}
+	fmt.Println(BsearchEnd(a, 4))
+}
+
+func TestBsearchEqFirst(t *testing.T) {
+	a := []int{1,2,3,4,4,4,4,6,6,6,6,667,7,5,6,7,8,9}
+	fmt.Println(BsearchEnd(a, 4))
+}
+
+func TestSearchAb(t *testing.T) {
+	fmt.Println("search 开始")
+	//a := []int{6,6,7,5,6,7,8,9,1,2,3,4,4,4,4,6,6}
+	//fmt.Println("searchAb", SearchAb(a))
+	b := []int{6,1,2,3,4,5,56}
+	fmt.Println("searchAb", SearchAb(b))
+}
+
+func TestMap(t *testing.T) {
+	data := map[string]interface{}{
+		"k":"val",
+	}
+	for t := range data {
+		fmt.Println(t)
+	}
+}
